6fb6738e3f652d1633154b5f889c106a312b24a7
Prabhnoor Singh <prabhnoorsingh.chowdhary@amadeus.com>
2025-10-16 08:43:48 +0000

    updated script and moving to tag

--- setup-work.sh
+++ setup-work.sh
@@ -8,4 +8,4 @@ kubectl -n argocd set image deployment argocd-redis redis=redis:7.2.7-alpine
 sleep 180
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null &
+#kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null &
